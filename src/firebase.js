acf2c20ad606e2181972c449047a5e92ab160540
Calvin See <calvinfrancissee@gmail.com>
2022-09-02 01:04:25 -0700

    Added firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+import { firebaseConfig } from './config.js';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const database = getDatabase();
