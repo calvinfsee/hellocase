c098d30b86a0fb96728d68994d4fb01c5041ba7c
Calvin See <calvinfrancissee@gmail.com>
2022-09-02 01:25:17 -0700

    Connected to firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,69 @@
+import { useState, useEffect, useCallback, useRef } from 'react';
+import { signInAnonymously, onAuthStateChanged } from 'firebase/auth';
+import { ref, set, onDisconnect, onValue, onChildAdded } from 'firebase/database';
 import './assets/stylesheets/App.css';
 import GameContainer from "./components/GameContainer.jsx";
+import { auth, database } from './firebase.js';
 
 export default function App() {
+  const [playerData, setPlayerData] = useState(null);
+
+  const [allPlayerRef, setAllPlayerRef] = useState(ref(database, `players`));
+  const [players, setPlayers] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const playerId = user.uid;
+        const playerRef = ref(database, `players/${playerId}`);
+        set(playerRef, {
+          id: playerId,
+          name: 'Calvin',
+          direction: 'right',
+          x: 2,
+          y: 2
+        });
+        onDisconnect(playerRef).remove();
+        setPlayerData({
+          id: playerId,
+          ref: playerRef
+        });
+      } else {
+        setPlayerData(null);
+      }
+    });
+
+    signInAnonymously(auth)
+    .then(() => {
+      console.log('signed in!');
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    onValue(allPlayerRef, (snapshot) => {
+      //Fires whenever change occurs
+      const newPlayers = snapshot.val() || {};
+      setPlayers(newPlayers);
+    });
+    onChildAdded(allPlayerRef, (snapshot) => {
+      //Fires whenever a new node is added to the tree
+      const addedPlayer = snapshot.val();
+      setPlayers((prev) => {
+        const newPlayerList = {...prev, addedPlayer};
+        return newPlayerList;
+      });
+    });
+  }, []);
 
   return (
     <div className="App">
-      <GameContainer />
+      <GameContainer players={players} />
     </div>
   )
 }
--- src/components/GameContainer.jsx
+++ src/components/GameContainer.jsx
@@ -3,17 +3,27 @@ import { useState, useEffect, useRef } from 'react';
 import useArrowKeys from '../hooks/useArrowKeys.jsx';
 import Player from './Player.jsx';
 
-export default function GameContainer () {
+export default function GameContainer ({ players, id }) {
   useArrowKeys();
 
-  const name = "Calvin";
-  const x = 2;
-  const y = -4;
-  const coor = `translate3d(${33 * x - 1}px, ${33 * y - 2}px, 0)`;
+  // const name = "Calvin";
+  // const x = 2;
+  // const y = -4;
+  // const coor = `translate3d(${33 * x - 1}px, ${33 * y - 2}px, 0)`;
+
+  function renderPlayers () {
+    const playerIds = Object.keys(players);
+    return playerIds.map((id) => {
+      const {name, direction, x, y} = players[id];
+      const coor = `translate3d(${33 * x - 1}px, ${33 * y - 2}px, 0)`;
+
+      return (<Player key={id + coor} name={name} dir={direction} coor={coor} />);
+    });
+  }
 
   return (
     <div id="game-container">
-      <Player name={name} coor={coor} />
+      {renderPlayers()}
     </div>
   )
 }
