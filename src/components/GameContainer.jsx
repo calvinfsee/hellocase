b8b8017bf86ca0ad0e5b98fad341f4ba31b4895d
Calvin See <calvinfrancissee@gmail.com>
2022-09-02 00:48:56 -0700

    Rendered Player sprite on screen

--- src/components/GameContainer.jsx
+++ src/components/GameContainer.jsx
@@ -1,13 +1,19 @@
 import "../assets/stylesheets/GameContainer.css";
 import { useState, useEffect, useRef } from 'react';
 import useArrowKeys from '../hooks/useArrowKeys.jsx';
+import Player from './Player.jsx';
 
 export default function GameContainer () {
   useArrowKeys();
 
+  const name = "Calvin";
+  const x = 2;
+  const y = -4;
+  const coor = `translate3d(${33 * x - 1}px, ${33 * y - 2}px, 0)`;
+
   return (
     <div id="game-container">
-
+      <Player name={name} coor={coor} />
     </div>
   )
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,21 +1,25 @@
-export default function Player ({ name, dir, coor, color}) {
+import { useMemo } from 'react';
+import '../assets/stylesheets/Player.css';
+
+export default function Player ({ name, coor}) {
   const coordinates = useMemo(() => {
     const styles = {
       transform: coor
     }
     return styles;
   }, [coor]);
+  //Player sprite is 32 x 48
 
 
 
 
   return (
-    <div className='Character grid-cell' style={coordinates} data-color={color} data-direction={dir}>
+    <div className='Character grid-cell' style={coordinates}>
       <div className="Character_sprite grid-cell"></div>
       <div className="Character_name-container">
         <span className="Character_name">{name}</span>
       </div>
-      <div className="Character_you-arrow"></div>
+      {/* <div className="Character_you-arrow"></div> */}
     </div>
   );
 }
